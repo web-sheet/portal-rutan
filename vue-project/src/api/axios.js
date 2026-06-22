737deabba5ed5c4d1a944ac8dacff6a16d9c4c43
Websheet <websheetweb@gmail.com>
2026-06-22 13:56:48 +0700

    publish awal

--- vue-project/src/api/axios.js
+++ vue-project/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-// console.log("API URL:", import.meta.env.VITE_API_URL);
-
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:8000/api',
+// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
